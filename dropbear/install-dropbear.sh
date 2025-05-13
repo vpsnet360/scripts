03b84b11a6a659086ebebf6a61f0c7b4ff587761
net_vps <101622761+vpsnet360@users.noreply.github.com>
2025-05-13 19:16:55 -0300

    Add files via upload

--- dropbear/install-dropbear.sh
+++ dropbear/install-dropbear.sh
@@ -1,1 +1,26 @@
+#!/bin/bash
+rm -rf installdrop.sh
+    echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+    echo -e "\E[41;1;37m                MINI INSTALADOR DROPBEAR 2019                 \E[0m"
+    echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+wget -q http://archive.ubuntu.com/ubuntu/pool/universe/d/dropbear/dropbear-bin_2019.78-2build1_amd64.deb 
 
+chmod +x dropbear-bin_2019.78-2build1_amd64.deb 
+
+dpkg -i dropbear-bin_2019.78-2build1_amd64.deb
+rm -rf dropbear-bin_2019.78-2build1_amd64.deb
+echo "PAQUETE DE SSH-2.0-DROPBEAR_2019.78 INSTALADO"
+read -p "INGRESE PUERTO :" port
+cat <<EOF >/etc/default/dropbear
+NO_START=0
+DROPBEAR_EXTRA_ARGS=" -p $port "
+DROPBEAR_RECEIVE_WINDOW=65536
+DROPBEAR_BANNER=""
+EOF
+echo "DROPBEAR CONFIGURADO OK"
+service dropbear restart
+echo "/bin/false" >> /etc/shells
+ufw allow $port
+    echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+    echo -e "\E[41;1;37m                ¡INSTALACIÓN COMPLETA!                 \E[0m"
+    echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
